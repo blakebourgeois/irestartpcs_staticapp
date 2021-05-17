7982435d57842e8125a76d586599ca8087d0311b
blakebourgeois <blake@quicksand.tech>
2021-05-16 19:30:50 -0500

    modifying style sheet, expanding stop generator

--- js/progress.js
+++ js/progress.js
@@ -2,7 +2,7 @@ function collecting(count){
     document.getElementById('progress').innerHTML = count;
     var countUp = setInterval(function(){
         if (count <= 100){
-            document.getElementById('progress').innerHTML = count + "% Complete";
+            document.getElementById('progress').innerHTML = count + "% complete";
             count++;
         }
     }, 666);
